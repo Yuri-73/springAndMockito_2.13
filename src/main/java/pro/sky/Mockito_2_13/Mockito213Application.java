a282eb051f689a8d809036492ad5af970114596e
Юрий <fedejkin@gmail.com>
2024-08-15 19:34:25 +0300

    Inicial commit

---
+++ src/main/java/pro/sky/Mockito_2_13/Mockito213Application.java
@@ -0,0 +1,13 @@
+package pro.sky.Mockito_2_13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mockito213Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mockito213Application.class, args);
+	}
+
+}
